58008b02c7f0b26cf31929ccb3263ab77b367615
Mobinzaidi12 <mobinzaidi2015@gmail.com>
2025-09-06 15:09:25 +0500

    complete

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_test_api/pages/ui_helper/ui_Helper.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -14,89 +15,139 @@ class _LoginState extends State<Login> {
   final _formKey = GlobalKey<FormState>();
   bool _isLoading = false;
 
+  Future<void> _login() async {
+    if(_formKey.currentState!.validate()){
+        setState(() {
+          _isLoading = true;
+        });
+
+        try{
+          var userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(email: emailController.text.trim(), password: passwordController.text.trim());
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Logged in successfully')),
+          );
+          Navigator.pushReplacementNamed(context, '/home');
+        }on FirebaseAuthException catch(e){
+          String message;
+          switch(e.code){
+            case 'user-not-found':
+              message = 'No user found for that email.';
+              break;
+            case 'wrong-password':
+              message = 'Wrong password provided for that user.';
+              break;
+            default:
+              message = e.message ?? 'An unknown error occurred';
+          }
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text(message)),
+          );
+
+        }finally{
+          setState(() {
+            _isLoading = false;
+          });
+        }
+    }
+  }
+
+@override void dispose(){
+    super.dispose();
+  emailController.dispose();
+  passwordController.dispose();
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       body: SafeArea(
         child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    'Login',
-                    style: TextStyle(
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(height: 60),
-                  Ui_Helper.customFormFiled(
-                    'Email',
-                    Icons.email,
-                    false,
-                    emailController,
-                      (value){
-                        if(value == null || value.isEmpty){
-                          return 'Please enter email';
-                        }
-                        if(!value.contains('@') || !value.contains('.')){
-                          return 'Please enter a valid email';
-                        }
-                        return null;
-                      }
-                  ),
-                  SizedBox(height: 30),
-                  Ui_Helper.customFormFiled(
-                    'Password',
-                    Icons.lock,
-                    true,
-                    passwordController,
-                      (value){
-                        if(value == null || value.isEmpty){
-                          return 'Please enter password';
-                        }
-                        if(value.length < 6){
-                          return 'Password must be at least 6 characters';
-                        }
-                        return null;
-                      }
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Text('Forgot Password?'),
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  Ui_Helper.customButton(
-                    'Login',
-                    Colors.blue,
-                    Colors.white,
-                    () {},
-                  ),
-                  SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("Don’t have an account? "),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.pushNamed(context, '/signup');
-                        },
-                        child: Text(
-                          "Sign up",
-                          style: TextStyle(color: Colors.blue),
-                        ),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.only(
+                left: 20,
+                right: 20,
+                bottom: MediaQuery.of(context).viewInsets.bottom,
+              ),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
                       ),
-                    ],
-                  ),
-                ],
+                    ),
+                    SizedBox(height: 60),
+                    Ui_Helper.customFormFiled(
+                      'Email',
+                      Icons.email,
+                      false,
+                      emailController,
+                        (value){
+                          if(value == null || value.isEmpty){
+                            return 'Please enter email';
+                          }
+                          if(!value.contains('@') || !value.contains('.')){
+                            return 'Please enter a valid email';
+                          }
+                          return null;
+                        }
+                    ),
+                    SizedBox(height: 30),
+                    Ui_Helper.customFormFiled(
+                      'Password',
+                      Icons.lock,
+                      true,
+                      passwordController,
+                        (value){
+                          if(value == null || value.isEmpty){
+                            return 'Please enter password';
+                          }
+                          if(value.length < 6){
+                            return 'Password must be at least 6 characters';
+                          }
+                          return null;
+                        }
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {},
+                        child: Text('Forgot Password?'),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    _isLoading
+                        ? const CircularProgressIndicator()
+                        : Ui_Helper.customButton(
+                      'Login',
+                      Colors.blue,
+                      Colors.white,
+                      _login,
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Don’t have an account? "),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pushNamed(context, '/signup');
+                          },
+                          child: Text(
+                            "Sign up",
+                            style: TextStyle(color: Colors.blue),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -75,103 +75,109 @@ class _SignupState extends State<Signup> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       body: SafeArea(
         child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    'Signup',
-                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 60),
-                  Ui_Helper.customFormFiled(
-                    'UserName',
-                    Icons.person,
-                    false,
-                    userNameController,
-                        (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter username';
-                      }
-                      if (value.length < 3) {
-                        return 'Username must be at least 3 characters';
-                      }
-                      return null;
-                    },
-
-                  ),
-                  SizedBox(height: 30),
-                  Ui_Helper.customFormFiled(
-                    'Email',
-                    Icons.email,
-                    false,
-                    emailController,
-                        (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter email';
-                      }
-                      if (!value.contains('@') || !value.contains('.')) {
-                        return 'Please enter a valid email';
-                      }
-                      return null;
-                    },
-
-                  ),
-                  SizedBox(height: 30),
-                  Ui_Helper.customFormFiled(
-                    'Password',
-                    Icons.lock,
-                    true,
-                    passwordController,
-                        (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter password';
-                      }
-                      if (value.length < 6) {
-                        return 'Password must be at least 6 characters';
-                      }
-                      return null;
-                    },
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Text('Forgot Password?'),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.only(
+                left: 20,
+                right: 20,
+                bottom: MediaQuery.of(context).viewInsets.bottom,
+              ),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Signup',
+                      style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
                     ),
-                  ),
-                  SizedBox(height: 20),
-                  _isLoading
-                      ? const CircularProgressIndicator()
-                      : Ui_Helper.customButton(
-                    'Signup',
-                    Colors.blue,
-                    Colors.white,
-                    _signUp,
-                  ),
-                  SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("Don’t have an account? "),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.pushNamed(context, '/login');
-                        },
-                        child: Text(
-                          "Login",
-                          style: TextStyle(color: Colors.blue),
-                        ),
+                    SizedBox(height: 60),
+                    Ui_Helper.customFormFiled(
+                      'UserName',
+                      Icons.person,
+                      false,
+                      userNameController,
+                          (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter username';
+                        }
+                        if (value.length < 3) {
+                          return 'Username must be at least 3 characters';
+                        }
+                        return null;
+                      },
+            
+                    ),
+                    SizedBox(height: 30),
+                    Ui_Helper.customFormFiled(
+                      'Email',
+                      Icons.email,
+                      false,
+                      emailController,
+                          (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter email';
+                        }
+                        if (!value.contains('@') || !value.contains('.')) {
+                          return 'Please enter a valid email';
+                        }
+                        return null;
+                      },
+            
+                    ),
+                    SizedBox(height: 30),
+                    Ui_Helper.customFormFiled(
+                      'Password',
+                      Icons.lock,
+                      true,
+                      passwordController,
+                          (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter password';
+                        }
+                        if (value.length < 6) {
+                          return 'Password must be at least 6 characters';
+                        }
+                        return null;
+                      },
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {},
+                        child: Text('Forgot Password?'),
                       ),
-                    ],
-                  ),
-                ],
+                    ),
+                    SizedBox(height: 20),
+                    _isLoading
+                        ? const CircularProgressIndicator()
+                        : Ui_Helper.customButton(
+                      'Signup',
+                      Colors.blue,
+                      Colors.white,
+                      _signUp,
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Don’t have an account? "),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pushNamed(context, '/login');
+                          },
+                          child: Text(
+                            "Login",
+                            style: TextStyle(color: Colors.blue),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
